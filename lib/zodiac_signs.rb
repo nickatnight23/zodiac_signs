2ee5a698d29b7342bd9fd8e19fa335e2343ff443
Latricia <tricianickelberry@gmail.com>
2020-02-14 08:07:00 -0800

    added api key

--- lib/zodiac_signs.rb
+++ lib/zodiac_signs.rb
@@ -2,8 +2,12 @@ require 'json'
 require 'rest-client'
 require 'pry'
 
+userID = "609442";
+apiKey = "cdbc28bfcceca218bb613fe53d98fc4d";
+
 
 require_relative "./zodiac_signs/version"
 require_relative './zodiac_signs/sign'
 require_relative './zodiac_signs/cli'
 require_relative './zodiac_signs/api'
+require_relative './zodiac_signs/sdk'
